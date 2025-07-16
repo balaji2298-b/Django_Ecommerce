73cc597d38bb08d1295545065eb6fd4836ac5f76
balaji <balajik1924@gmail.com>
2025-07-16 14:09:37 +0530

    Update wsgi.py

--- djangoecommerce/ecommerce/ecommerce/wsgi.py
+++ djangoecommerce/ecommerce/ecommerce/wsgi.py
@@ -7,10 +7,22 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
+
+# application = get_wsgi_application()
+import sys
 import os
 
+# Replace with your path
+path = '/home/balajibalajik/Django_Ecommerce'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'ecommerce.settings'
+
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
-
 application = get_wsgi_application()
